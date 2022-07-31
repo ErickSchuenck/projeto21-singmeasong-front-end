fc8666eb0aee04277384d11462fda26a0854a65e
ErickSchuenck <esfmokdeci@gmail.com>
2022-07-31 16:42:47 -0300

    cypress create recommendation

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,20 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/// <reference types='cypress'/>
+import { faker } from "@faker-js/faker"
+
+const text = faker.music.songName()
+const link = 'https://www.youtube.com/watch?v=nNhMjV76OQo'
+
+
+describe("Home page", () => {
+
+  it('should create recommendation', () => {
+    cy.visit('http://localhost:3000');
+    cy.get("#createRecommendationNameInput").type(`${text}`);
+    cy.get("#createRecommendationLinkInput").type(link);
+    cy.wait(1000);
+    cy.get("#button").click();
+  });
+
+  it("should return an alert when registering an existing recommendation", () => {
+  });
+});
