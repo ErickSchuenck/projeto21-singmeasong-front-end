262d00753e587ac18ac3bf0458997b35b04d57c8
ErickSchuenck <esfmokdeci@gmail.com>
2022-08-01 13:59:22 -0300

    reset database not working

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,6 +4,11 @@ import { faker } from "@faker-js/faker"
 const text = faker.music.songName()
 const link = 'https://www.youtube.com/watch?v=nNhMjV76OQo'
 
+// FIXME // not working right
+
+// beforeEach(() => {
+//   cy.resetDatabase();
+// });
 
 describe("Home page", () => {
 
@@ -56,12 +61,38 @@ describe("Home page", () => {
   });
 
   it("should exclude recommendation", () => {
-
-    // it only works if database starts empty, should be fixed
-
     for (let i = 0; i < 6; i++) {
       cy.get("#downvoteButton").first().click();
     }
+
+    // this line below only works if database starts empty, should be fixed
     cy.contains("No recommendations yet! Create your own :)");
+  });
+});
+
+describe('Random', () => {
+  const musicName = text;
+
+  it('should create a recommendation to be shown in the random recommendation suite', () => {
+    // cy.visit('http://localhost:3000');
+    cy.get("#createRecommendationNameInput").type(`${musicName}`);
+    cy.get("#createRecommendationLinkInput").type(link);
+    cy.wait(1000);
+    cy.get("#button").click();
+  });
+
+  it('should show a random recommendation', () => {
+    cy.visit('http://localhost:3000/random');
+    cy.intercept('GET', '/recommendations/random').as('randomRecommendation');
+    cy.wait('@randomRecommendation');
+    cy.get('article').should('have.length', 1);
+  });
+
+  it("should exclude the random recommendation", () => {
+    cy.visit('http://localhost:3000');
+    cy.wait(1000);
+    for (let i = 0; i < 6; i++) {
+      cy.get("#downvoteButton").click();
+    }
   });
 });
